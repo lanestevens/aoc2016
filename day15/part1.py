600d08979f9c0afa23b4041b533f611d562c8db7
Lane Stevens <lane@computer.org>
2016-12-14 23:52:10 -0700

    Day 15

---
+++ day15/part1.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+discs = [(1, 13),
+         (10, 19),
+         (2, 3),
+         (1, 7),
+         (3, 5),
+         (5, 17),
+         ]
+
+def advance_time(l):
+    new_l = []
+    for item in l:
+        new_l.append(((item[0] + 1) % item[1], item[1]))
+    return new_l
+
+def check_slots(l):
+    current = l[:]
+    for i in range(len(l)):
+        current = advance_time(current)
+        if current[i][0]:
+            return False
+
+    return True
+
+def find_time(l):
+    i = 0
+    while True:
+        print i
+        print l
+        if check_slots(l):
+            return i
+        l = advance_time(l)
+        i += 1
+
+print find_time(discs)
+
+         
